0bf6373a5625abba28a4caac9f076ce50e1316cb
Zhurks01~ <Vanessa_99@mail.ru>
2023-04-06 22:48:53 +0500

    added services section

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 $(function () {
+    $('.services__slider').slick({
+        prevArrow: "<img src='../images/services-filter/arrow-left.png' class='prev' alt='previous arrow'>",
+        nextArrow: "<img src='../images/services-filter/arrow-right.png' class='next' alt='next arrow'>",
+    });
 
-      
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
     });
