98fbc6bdc56c27ee88644bcd14398921c0c1d1b8
Zhurks01~ <Vanessa_99@mail.ru>
2023-04-08 23:52:23 +0500

    added portfolio section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,7 @@
 $(function () {
+
+    var mixer = mixitup('.portfolio__items');
+
     $('.services__slider').slick({
         prevArrow: "<img src='../images/services-filter/arrow-left.png' class='prev' alt='previous arrow'>",
         nextArrow: "<img src='../images/services-filter/arrow-right.png' class='next' alt='next arrow'>",
@@ -7,4 +10,5 @@ $(function () {
     $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active');
     });
+
 });
