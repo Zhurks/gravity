b755b73f2d639c1f279630f466b12c637c0de847
Zhurks01~ <Vanessa_99@mail.ru>
2023-04-14 21:30:49 +0500

    added all sections

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,13 @@ $(function () {
     var mixer = mixitup('.portfolio__items');
 
     $('.services__slider').slick({
-        prevArrow: "<img src='../images/services-filter/arrow-left.png' class='prev' alt='previous arrow'>",
-        nextArrow: "<img src='../images/services-filter/arrow-right.png' class='next' alt='next arrow'>",
+        prevArrow: "<img src='../images/services-filter/arrow-left.png' class='prev services__prev' alt='previous arrow'>",
+        nextArrow: "<img src='../images/services-filter/arrow-right.png' class='next services__next' alt='next arrow'>",
+    });
+
+    $('.testimonials__slider').slick({
+        prevArrow: "<img src='../images/services-filter/arrow-left.png' class='prev testimonials__prev' alt='previous arrow'>",
+        nextArrow: "<img src='../images/services-filter/arrow-right.png' class='next testimonials__next' alt='next arrow'>",
     });
 
     $('.menu__btn').on('click', function () {
